0cc65badfe0ee83632f43349cced6f0af7c4b025
IlyesOul <ouldsaada.ilyes@gmail.com>
2024-08-11 12:07:51 -0400

    Finalized Base Visualization

--- learning/RFE_simple.py
+++ learning/RFE_simple.py
@@ -1,9 +1,21 @@
 from data_collection.continuous import simple_time_series
 import learning_interface
+from sklearn.ensemble import GradientBoostingRegressor
+import matplotlib
+
 
 class rfe_simple(learning_interface):
 
     def __init__(self, ticker):
         self.ticker = ticker
 
-# Allahukbar
+    # Function that handles data initialization in 'data.csv'
+    def data_conversion(self):
+        converter = simple_time_series.time_series_converter()
+        converter.attain_data('data.json')
+        converter.convert_data('data.csv')
+
+
+    def training_optimization(self):
+
+        ensemble = GradientBoostingRegressor()
