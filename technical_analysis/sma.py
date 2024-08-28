84f19230858a5a6b2e0b7d9833edef50219f6a8a
Mohamed Bouchtout <mohamedbouchtout@Mohameds-Air.fios-router.home>
2024-08-28 10:52:39 -0400

    code

--- technical_analysis/sma.py
+++ technical_analysis/sma.py
@@ -2,7 +2,8 @@ from alpha_vantage.techindicators import TechIndicators
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 from data_collection import continuous
-
+from datetime import datetime
+import pytz
 
 # Your Alpha Vantage API Key
 Key = '1WCNX62EOULHYZUW'
