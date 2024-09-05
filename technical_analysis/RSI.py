501bd77c968faadb84e859f6cd7a6fc022e35a59
tg-24 <tmghile05@gmail.com>
2024-09-05 13:24:04 -0400

    Finished RSI.py

--- technical_analysis/RSI.py
+++ technical_analysis/RSI.py
@@ -6,14 +6,20 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-
+interlist=['60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']
 class RSI_Indicator():
     def __init__(self,symbol,time_period, interval, series_type):
         self.ticker_symbol=symbol
         self.time=time_period
         self.inter=interval
         self.series=series_type
-        self.data=yf.download(self.ticker_symbol,start='2023-01-01',end='2024-01-01')
+
+        while not self.inter in interlist:
+            print("Interval is not valid!")
+            self.inter=input(f"Input a new interval from this range {interlist} ")
+
+        self.data=yf.download(self.ticker_symbol,start='2023-01-01',end='2024-01-01',interval=self.inter)
+
         self.filename='Stock_Historical_Data.xlsx'
         self.excel_file=os.path.join(os.getcwd(),'technical_analysis\\',self.filename)
         self.data.to_excel(self.excel_file,sheet_name='Historical Data',engine='openpyxl')
@@ -25,6 +31,7 @@ class RSI_Indicator():
         self.data = pd.read_excel(self.excel_file)
         self.data[self.label]=ta.RSI(self.data[self.series],self.time)
         describe=self.data.describe()
+
         if describe.iloc[1,7].round()<=30:
             print("Stock is oversold and undervalued! RSI indicates a good time to buy.")
         elif describe.iloc[1,7].round()<70:
@@ -40,6 +47,6 @@ class RSI_Indicator():
     #     plt.show()
 
 if __name__ in '__main__':
-    share=RSI_Indicator('AAPL',5,60,'Close')
+    share=RSI_Indicator('NVDA',5,'100m','Close')
     share.load_data()
     
