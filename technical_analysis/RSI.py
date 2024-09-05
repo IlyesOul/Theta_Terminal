ff8321d18d15f0a14d2984dbe944b93a94dfcba6
tg-24 <tmghile05@gmail.com>
2024-09-04 21:25:40 -0400

    Finished RSI.py

--- technical_analysis/RSI.py
+++ technical_analysis/RSI.py
@@ -14,7 +14,8 @@ class RSI_Indicator():
         self.inter=interval
         self.series=series_type
         self.data=yf.download(self.ticker_symbol,start='2023-01-01',end='2024-01-01')
-        self.excel_file='C:\\Users\\tmghi\\Theta_Terminal\\technical_analysis\\Stock_Historical_Data.xlsx'
+        self.filename='Stock_Historical_Data.xlsx'
+        self.excel_file=os.path.join(os.getcwd(),'technical_analysis\\',self.filename)
         self.data.to_excel(self.excel_file,sheet_name='Historical Data',engine='openpyxl')
         self.label='RSI_'+str(self.time)
     
@@ -23,14 +24,20 @@ class RSI_Indicator():
         print(f'Data exported to {self.excel_file}')
         self.data = pd.read_excel(self.excel_file)
         self.data[self.label]=ta.RSI(self.data[self.series],self.time)
-        print(self.data)
-        self.graph()
+        describe=self.data.describe()
+        if describe.iloc[1,7].round()<=30:
+            print("Stock is oversold and undervalued! RSI indicates a good time to buy.")
+        elif describe.iloc[1,7].round()<70:
+            print("Stock is neutral! RSI indicates not a good time to buy or sell.")
+        else:
+            print("Stock is overbought and overvalued! RSI indicates a good time to sell.")
+
         
-    def graph(self):
-        plt.plot(self.data[self.label])
-        plt.xlabel('Index')
-        plt.ylabel('RSI Values')
-        plt.show()
+    # def graph(self):
+    #     plt.plot(self.data[self.label])
+    #     plt.xlabel('Index')
+    #     plt.ylabel('RSI Values')
+    #     plt.show()
 
 if __name__ in '__main__':
     share=RSI_Indicator('AAPL',5,60,'Close')
