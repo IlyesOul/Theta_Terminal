e27f8a009e7e1212f14824e854e2e7fd8078a197
tg-24 <tmghile05@gmail.com>
2024-09-17 12:00:50 -0400

    Finished RSI.py

--- technical_analysis/RSI.py
+++ technical_analysis/RSI.py
@@ -6,7 +6,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-interlist=['60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']
+
+interlist=['1m','2m','5m','15m','30m','60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']
+
 class RSI_Indicator():
     def __init__(self,symbol,time_period, interval, series_type):
         self.ticker_symbol=symbol
@@ -31,22 +33,23 @@ class RSI_Indicator():
         self.data = pd.read_excel(self.excel_file)
         self.data[self.label]=ta.RSI(self.data[self.series],self.time)
         describe=self.data.describe()
-
         if describe.iloc[1,7].round()<=30:
             print("Stock is oversold and undervalued! RSI indicates a good time to buy.")
         elif describe.iloc[1,7].round()<70:
             print("Stock is neutral! RSI indicates not a good time to buy or sell.")
         else:
             print("Stock is overbought and overvalued! RSI indicates a good time to sell.")
+        
+        self.graph()
 
         
-    # def graph(self):
-    #     plt.plot(self.data[self.label])
-    #     plt.xlabel('Index')
-    #     plt.ylabel('RSI Values')
-    #     plt.show()
+    def graph(self):
+        plt.plot(self.data[self.label])
+        plt.xlabel('Index')
+        plt.ylabel('RSI Values')
+        plt.show()
 
 if __name__ in '__main__':
-    share=RSI_Indicator('NVDA',5,'100m','Close')
+    share=RSI_Indicator('NVDA',5,'60m','Close')
     share.load_data()
     
